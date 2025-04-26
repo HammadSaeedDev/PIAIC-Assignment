e0fe862c0f999d752c7f6f49b35b015c29448631
HammadSaeedDev <162806425+HammadSaeedDev@users.noreply.github.com>
2025-04-26 19:15:56 +0500

    Update class_ques_batch_75.py

---
+++ class_ques_batch_75.py
@@ -0,0 +1,90 @@
+# Question no 01
+
+length = 15
+breadth = 10
+
+area = length * breadth
+
+print("Area of the rectangle:", area)
+
+# Question no 02
+
+fahrenheit = 98.6
+
+celsius = (fahrenheit - 32) * 5/9
+
+print("Temperature in Celsius:", celsius)
+
+# Question no 03
+
+math = 87
+science = 92
+english = 78
+
+total_marks = math + science + english
+average_marks = total_marks / 3
+
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
+
+# Question no 04
+
+side = 12
+
+perimeter = 4 * side
+
+print("Perimeter of the square:", perimeter)
+
+# Question no 05
+
+principal = 10000
+rate = 5
+time = 2
+
+simple_interest = (principal * rate * time) / 100
+
+print("Simple Interest is:", simple_interest)
+
+# Question no 06
+
+height_in_feet = 5.5
+
+height_in_inches = height_in_feet * 12
+
+print("Height in inches:", height_in_inches)
+
+# Question no 07
+
+radius = 7
+pi = 3.14
+
+area = pi * radius * radius
+
+print("Area of the circle:", area)
+
+# Question no 08
+
+days = 3
+
+hours = days * 24
+minutes = hours * 60
+
+print("Hours:", hours)
+print("Minutes:", minutes)
+
+# Question no 09
+
+side = 9
+
+volume = side ** 3
+
+print("Volume of the cube:", volume)
+
+# Question no 10
+
+weight = 70  
+height = 1.75
+
+bmi = weight / (height ** 2)
+
+print("BMI is:", round(bmi , 2))
